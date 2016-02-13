391fa440d5408d5e900ced36137416be0325f3f8
Jefferson Lim <jefferer@gmail.com>
2016-02-13 14:38:01 -0800

    [feature] google places auto suggest now stores into storage

--- client/auth/signup/signupController.js
+++ client/auth/signup/signupController.js
@@ -1,14 +1,20 @@
 angular.module('auth.signup', [])
 .controller('SignupController', function($location, $window, AuthServices, $scope, $http) {
-
+	// $scope.user = {};
 	var user = {};
 	var signup = this;
+	var places;
+
+	// var city;
 
 	signup.submit = function(){
 		user.username = signup.username;
 	 	user.password = signup.password;
 	 	user.email = signup.email;
-	 	user.city = signup.city;
+	 	//user.city.name = only zipcode
+	 	//user.city.formatted_address = city,zip,us
+	 	user.city = places.address_components[1].long_name;
+	 	console.log('+++line 13:', places)
 	 	AuthServices.submitNewUser({user: user})
 	 		.then(function(token){
 	 		  if(token){
@@ -33,11 +39,22 @@ angular.module('auth.signup', [])
 	            country: "usa"
 	        }
 	    }
+
 	    var autocomplete = new google.maps.places.Autocomplete(input, options);
 
+	    autocomplete.addListener('place_changed', function() {
+	    	places = autocomplete.getPlace();
+	    });
+
+
 	}
-		google.maps.event.addDomListener(window, 'load', postal_code);
+
+	$scope.init = function() {
 		postal_code();
+		google.maps.event.addDomListener(window, 'load', postal_code);
+	}	
+
+	$scope.init();
 
 
 })
